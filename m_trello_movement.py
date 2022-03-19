038affa9c6e7f846dfd6eba832bb4c12600235b8
Sunny Lim <limhss@gmail.com>
2022-03-19 17:37:06 +0800

    S

--- m_trello_movement.py
+++ m_trello_movement.py
@@ -39,7 +39,7 @@ res = summary.fetch(query = None, limit=1000, last=None)
 output = pd.DataFrame()
 for col in res.items:
     st.write(type(int(col['mov_in'])-int(col['mov_out'])), int(col['mov_in'])-int(col['mov_out']))
-    col['on_list'] = 3
+    col['on_list'] = int(col['mov_in'])-int(col['mov_out'])
     output = output.append(col, ignore_index=True)
 
 output.columns = ['Date', 'Move In', 'Move Out', 'On List']
