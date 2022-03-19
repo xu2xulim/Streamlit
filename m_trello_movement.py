2bf16a8160162c335590978ac6cfb2e78193a7b8
Sunny Lim <limhss@gmail.com>
2022-03-19 15:27:54 +0800

    S

--- m_trello_movement.py
+++ m_trello_movement.py
@@ -9,7 +9,7 @@ import json
 deta = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG")
 
 # 3) create and use as many DBs as you want!
-db = deta.Base("trello_base")
+db = deta.Base("trello_movement")
 
 res = db.fetch(query = None, limit=1000, last=None)
 
