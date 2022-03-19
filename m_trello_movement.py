2f2a8e6d1211c3e0e31e0db80c2f7d99b7fde0ee
Sunny Lim <limhss@gmail.com>
2022-03-19 10:23:22 +0800

    Added some code

--- m_trello_movement.py
+++ m_trello_movement.py
@@ -8,3 +8,13 @@ deta = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG")
 db = deta.Base("trello_base")
 
 res = db.fetch(query = None, limit=1000, last=None)
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = res.items
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
+st.subheader('Raw data')
+st.write(data)
