a906650c1a59c8a5d920b24cfbb70053381424a1
Sunny Lim <limhss@gmail.com>
2022-03-19 12:33:28 +0800

    S

--- m_trello_movement.py
+++ m_trello_movement.py
@@ -25,7 +25,7 @@ req_columns = [[col['listAfter'], col['listBefore']] for col in res.items]
 df = pd.DataFrame (req_columns, columns = ['After', 'Before'])
 st.write(df)
 req = df.loc[df['After'] == 'Up Next']
-st.write(req.count_values())
+st.metric(df['After'].groupby('Up Next').count())
 
 #st.subheader('Raw data')
 
