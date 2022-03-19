78a871d339005f5210b4f6bf5d5ef1ebfe150e57
Sunny Lim <limhss@gmail.com>
2022-03-19 14:50:47 +0800

    S

--- m_trello_movement.py
+++ m_trello_movement.py
@@ -19,20 +19,15 @@ data_load_state = st.text('Loading data...')
 data = res.items
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
-st.dataframe(data=data, width=None, height=None)
+#st.dataframe(data=data, width=None, height=None)
 req_columns = [[col['listAfter'], col['listBefore']] for col in res.items]
 
 df = pd.DataFrame (req_columns, columns = ['After', 'Before'])
-st.write(df)
+#st.write(df)
 mov_in = df.loc[df['After'] == 'Up Next']
 mov_out = df.loc[df['Before'] == 'Up Next']
-col1, col2, col3 = st.columns(3)
-col1.metric(label="Move In", value=mov_in[mov_in.columns[0]].count())
-col2.metric(label="Move Out", value=mov_out[mov_out.columns[0]].count())
-col3.metric(label="On List", value=(mov_in[mov_in.columns[0]].count()-mov_out[mov_out.columns[0]].count()))
-
-#
-
-#hist_values = np.histogram(df)
-#st.bar_chart(data=df)
-#st.table(data)
+col1, col2, col3, col4 = st.columns(4)
+col1.metric(label="List", value='Up Next')
+col2.metric(label="Move In", value=mov_in[mov_in.columns[0]].count())
+col3.metric(label="Move Out", value=mov_out[mov_out.columns[0]].count())
+col4.metric(label="On List", value=(mov_in[mov_in.columns[0]].count()-mov_out[mov_out.columns[0]].count()))
