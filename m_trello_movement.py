f6421e824c89c1dcce86387ebdf9399558c3249a
Sunny Lim <limhss@gmail.com>
2022-03-19 12:26:50 +0800

    S

--- m_trello_movement.py
+++ m_trello_movement.py
@@ -24,7 +24,7 @@ req_columns = [[col['listAfter'], col['listBefore']] for col in res.items]
 
 df = pd.DataFrame (req_columns, columns = ['After', 'Before'])
 st.write(df)
-req = pd.loc[(df['After'] == 'Up Next')]
+req = pd.loc[df['After'] == 'Up Next']
 st.write(req)
 
 #st.subheader('Raw data')
