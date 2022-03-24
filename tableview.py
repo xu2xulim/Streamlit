37c0ffb147f3e0e02b1ed326b932851c24bedbb3
Sunny Lim <limhss@gmail.com>
2022-03-24 14:14:42 +0800

    S

---
+++ tableview.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from datetime import datetime
+from deta import Deta
+import json
+
+# 2) initialize with a project key
+deta = Deta(st.secrets["tableview"])
+
+# 3) create and use as many DBs as you want!
+db = deta.Base(st.secrets["MOVEMENT"])
+today = datetime.today().strftime('%Y-%m-%d')
+#res = db.fetch(query = {'date' : today}, limit=1000, last=None)
+
+# Create a text element and let the reader know the data is loading.
+#data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+#data = res.items
+# Notify the reader that the data was successfully loaded.
+
+#st.dataframe(data=data, width=None, height=None)
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
