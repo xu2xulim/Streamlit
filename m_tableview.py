8b2db0c85ad576ad1504ecbbc2bdbaf044cb2592
Sunny Lim <limhss@gmail.com>
2022-03-24 14:53:11 +0800

    S

--- m_tableview.py
+++ m_tableview.py
@@ -29,17 +29,15 @@ data = res.json()
 board_list = (",".join(data['name'].keys()))
 
 
-option = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    #('Email', 'Home phone', 'Mobile phone')
-    board_list
+
 )
 
 if option == 'Email' :
     st.write('You selected:', option)
 else:
 # Add a slider to the sidebar:
-    add_slider = st.sidebar.slider(
-        'Select a range of values',
-        0.0, 100.0, (25.0, 75.0)
-        S)
+    option = st.sidebar.selectbox(
+        'How would you like to be contacted?',
+        #('Email', 'Home phone', 'Mobile phone')
+        board_list
+    )
