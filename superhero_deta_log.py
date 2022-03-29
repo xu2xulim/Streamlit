5a83090a6bb7ecfcfe099641844ad907c843a335
Sunny Lim <limhss@gmail.com>
2022-03-29 12:35:09 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -11,4 +11,5 @@ log = alert = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base('superhero_
 
 res = log.fetch(query=None, limit=None, last=None)
 
-st.write(res.items)
+df = pd.Dataframe(res.items)
+st.dataframe(df)
