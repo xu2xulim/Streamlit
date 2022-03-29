b50a261f7a414d181ed51059e24bdc2cf2c3b53b
Sunny Lim <limhss@gmail.com>
2022-03-29 14:02:08 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -12,5 +12,6 @@ log = alert = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base('superhero_
 res = log.fetch(query=None, limit=None, last=None)
 
 df = pd.DataFrame.from_dict(res.items)
-endpoint_mbr = df.groupby(['endpoint', 'mbr_id']).count().fillna(0)
+df['date']=df['datetime'][0:10]
+endpoint_mbr = df.groupby(['endpoint', 'mbr_id', 'date']).count().fillna(0)
 st.dataframe(endpoint_mbr)
