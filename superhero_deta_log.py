6531a960d82139d380c7fcf5698cbf9ab61ff344
Sunny Lim <limhss@gmail.com>
2022-03-29 12:02:40 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -7,8 +7,8 @@ from deta import Deta
 import json
 import requests
 
-log = alert = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base('superhero_log')
+#log = alert = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base('superhero_log')
 
-res = log.fetch(query=None, limit=None, last=None)
+#res = log.fetch(query=None, limit=None, last=None)
 
-st.write(res)
+st.write('Something')
