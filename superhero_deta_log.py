f9840d8f4a15acf4ce19be58868b661f1f1b6e96
Sunny Lim <limhss@gmail.com>
2022-03-29 18:25:39 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -50,6 +50,7 @@ df3.set_index('date')
 
 for ix in range(0, len(df2.index)) :
     st.write(df2.iloc[ix]['date'])
+    st.write(df2.iloc[ix]['key'])
     dd = {}
     if df2.iloc[ix]['date'] not in df3['date'].keys() :
         dd = {'date': df2.iloc[ix]['date']}
