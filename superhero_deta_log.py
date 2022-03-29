bda49487a1bbd05d07e678b84ef24ec02eb433cc
Sunny Lim <limhss@gmail.com>
2022-03-29 13:43:36 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -12,5 +12,5 @@ log = alert = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base('superhero_
 res = log.fetch(query=None, limit=None, last=None)
 
 df = pd.DataFrame.from_dict(res.items)
-endpoint_mbr = df.groupby({['endpoint', 'mbr_id']}).size().unstack()
+endpoint_mbr = df.groupby(['endpoint', 'mbr_id']).size().unstack()
 st.bar_chart(data=endpoint_mbr)
