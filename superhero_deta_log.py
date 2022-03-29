a784c0d099f8a6b4cbfb6dfb26501d97f3aa07f6
Sunny Lim <limhss@gmail.com>
2022-03-29 18:59:31 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -52,7 +52,7 @@ for ix in range(0, len(df2.index)) :
     st.write(df2.iloc[ix]['date'])
     st.write(df2.iloc[ix]['key'])
     dd = {}
-    if df2.iloc[ix]['date'] in df3.loc[:,"date"]:
+    if df3['date'] == df2.iloc[ix]['date']:
         st.write('Found')
         dd = {'date': df2.iloc[ix]['date']}
         df3 = df3.append(dd, ignore_index = True)
