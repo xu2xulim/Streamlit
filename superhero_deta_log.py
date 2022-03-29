d198bdaa3a8f770e52e5c33b65bd48c9d22823ff
Sunny Lim <limhss@gmail.com>
2022-03-29 14:52:58 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -21,6 +21,6 @@ df1 = df.groupby(['endpoint', 'mbr_id', 'date']).count()
 #grouped_multiple.columns = ['endpoint', 'mbr_id', 'date', 'count']
 df1 = df1.reset_index()
 df2 = df1[['endpoint', 'mbr_id', 'date', 'key']]
-st.dataframe(df2)
+#st.dataframe(df2)
 
-st.bar_chart(df2[['date','key']])
+st.dataframe(df2[['date','key']])
