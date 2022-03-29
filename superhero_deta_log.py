3e749c01a2472a5b78193e6d5cfd885a431633b4
Sunny Lim <limhss@gmail.com>
2022-03-29 18:26:54 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -56,7 +56,7 @@ for ix in range(0, len(df2.index)) :
         dd = {'date': df2.iloc[ix]['date']}
         df3 = df3.append(dd, ignore_index = True)
     else:
-        dd = {df2.iloc[ix]['mbr_id'] : df2.iloc[ix]['key'] }
+        dd = {df2.iloc[ix]['mbr_id'] : df2.iloc[ix]['key'].values }
         st.write(dd)
         df3.loc[df2.iloc[ix]['date']].append(dd, ignore_index)
 
