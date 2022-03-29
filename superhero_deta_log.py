94ac78115451b46b2b387ac8ca8e12bdc89c67bf
Sunny Lim <limhss@gmail.com>
2022-03-29 11:56:47 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+from datetime import datetime
+from deta import Deta
+import json
+import requests
+
+log = alert = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base('superhero_log')
+
+res = log.fetch(query=None, limit=None, last=None)
+
+st.write(res)
