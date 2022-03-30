7b6c45304384aac8b948336564a17f2f7f12190d
Sunny Lim <limhss@gmail.com>
2022-03-30 13:20:48 +0800

    S

--- superhero_deta_log.py
+++ superhero_deta_log.py
@@ -16,18 +16,32 @@ log = alert = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base('superhero_
 res = log.fetch(query=None, limit=None, last=None)
 
 dd = {}
+##Start
 for row in unique([x['mbr_id'] for x in res.items]) :
     if row not in dd.keys():
         dd[row] = {}
     for y in unique([x['datetime'][0:10] for x in res.items]):
         dd[row][y] = 0
-st.write(dd)
+
 for z in res.items :
     dd[z['mbr_id']][z['datetime'][0:10]] = dd[z['mbr_id']][z['datetime'][0:10]] + 1
-dx = pd.DataFrame.from_dict(dd)
-st.dataframe(dx)
-st.bar_chart(dx)
+d_mbr = pd.DataFrame.from_dict(dd)
+st.header('Daily usage by member')
+st.dataframe(d_mbr)
+st.bar_chart(d_mbr)
+##Start
+for row in unique([x['endpoint'] for x in res.items]) :
+    if row not in dd.keys():
+        dd[row] = {}
+    for y in unique([x['datetime'][0:10] for x in res.items]):
+        dd[row][y] = 0
 
+for z in res.items :
+    dd[z['endpoint']][z['datetime'][0:10]] = dd[z['endpoint]][z['datetime'][0:10]] + 1
+d_endpoint = pd.DataFrame.from_dict(dd)
+st.header('Daily usage by endpoint')
+st.dataframe(d_endpoint)
+st.bar_chart(d_endpoint)
 
 
 df = pd.DataFrame.from_dict(res.items)
