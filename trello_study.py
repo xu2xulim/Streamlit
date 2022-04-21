dd90841207585aeb4dd8b3442964199bd2ff34fb
Sunny Lim <limhss@gmail.com>
2022-04-21 14:37:21 +0800

    S

--- trello_study.py
+++ trello_study.py
@@ -13,11 +13,10 @@ import urllib.parse
 #order = Deta(st.secrets["DETA_PROJECT_ID"]).Base("trello_orders")
 st.header("Trello Study")
 
-if st.session_state['focus'] == 1:
-    with st.expander("Open to enter order details"):
-        data = {'key' : st.secret('TRELO_API_KEY'), 'token' : st.secret('TRELO_TOKEN')}
-        url_values = urllib.parse.urlencode(data)
-        url = "https://trello.com/b/SsRevba7/project-archives.json?{}".format(url_values)
-        result = urllib.request.urlopen(url)
-        json_obj = json.loads(result.read().decode('utf-8'))
-        st.json(json_obj)
+with st.expander("Open to enter order details"):
+    data = {'key' : st.secret('TRELO_API_KEY'), 'token' : st.secret('TRELO_TOKEN')}
+    url_values = urllib.parse.urlencode(data)
+    url = "https://trello.com/b/SsRevba7/project-archives.json?{}".format(url_values)
+    result = urllib.request.urlopen(url)
+    json_obj = json.loads(result.read().decode('utf-8'))
+    st.json(json_obj)
