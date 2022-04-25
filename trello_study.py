f53bcd76321f8e61b7245daabb230bde96d1aeba
Sunny Lim <limhss@gmail.com>
2022-04-25 10:41:39 +0800

    S

--- trello_study.py
+++ trello_study.py
@@ -12,6 +12,9 @@ import urllib.request
 import urllib.parse
 from trello import TrelloClient, List
 
+query_params = st.experimental_get_query_params()
+st.write(query_params)
+
 @st.cache(suppress_st_warning=True)
 def trello_client(key, tkn):
     client = TrelloClient(
@@ -21,11 +24,12 @@ def trello_client(key, tkn):
     mbr_id = client.fetch_json('members/me')['id']
     return (client, mbr_id)
 #order = Deta(st.secrets["DETA_PROJECT_ID"]).Base("trello_orders")
-query_params = st.experimental_get_query_params()
+
 st.header("Trello Study")
 (client, me) = trello_client(st.secrets['TRELLO_API_KEY'], st.secrets['TRELLO_TOKEN'])
 card = client.get_card(query_params['card_id'])
 st.header(card.name)
+
 with st.expander("Open to see card labels"):
     #data = {'key' : st.secrets['TRELLO_API_KEY'], 'token' : st.secrets['TRELLO_TOKEN']}
     #url_values = urllib.parse.urlencode(data)
