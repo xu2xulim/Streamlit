d00d2c4e8e48679b76f147200c7266647487ab9a
Sunny Lim <limhss@gmail.com>
2022-04-30 19:04:57 +0800

    S

---
+++ streamlit_test.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+import streamlit.components.v1 as components
+
+components.html('''<blockquote class="trello-card-compact">
+  <a href="https://trello.com/c/AKtsBUPw/79-setup-your-smtp-on-contalist-and-test">Trello Card</a>
+</blockquote>
+<script src="https://p.trellocdn.com/embed.min.js"></script>''')
