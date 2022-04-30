438a680bea539c099025c4b80557f5b6761684da
Sunny Lim <limhss@gmail.com>
2022-04-30 19:08:27 +0800

    S

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
 import streamlit.components.v1 as components
-
+st.write("Something below")
 components.html('''<blockquote class="trello-card-compact">
   <a href="https://trello.com/c/AKtsBUPw/79-setup-your-smtp-on-contalist-and-test">Trello Card</a>
 </blockquote>
 <script src="https://p.trellocdn.com/embed.min.js"></script>''')
+
+st.write("Something above")
