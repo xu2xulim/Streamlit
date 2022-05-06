4e31b86cc31b97535c500db70dae53ce4e61796b
Sunny Lim <limhss@gmail.com>
2022-05-06 10:53:57 +0800

    S

--- trello_study.py
+++ trello_study.py
@@ -178,10 +178,10 @@ with st.expander("Open to read card description"):
 with st.expander("Open to inspect custom fields on card"):
     res = requests.post('https://cs0kji.deta.dev/card_customfields', json={"card_id" : card_id})
     if res.status_code == 200 :
-        customfields = pd.DataFrame(res.json()['customfields'])
+        #customfields = pd.DataFrame(res.json()['customfields'])
         #customfields = df.set_index(['Name', 'Value'])
-        st.write(customfields)
-        #st.json(res.json())
+        #st.write(customfields)
+        st.json(res.json()['customfields'])
 
 with st.expander("Open to see status of checklists on card"):
 
