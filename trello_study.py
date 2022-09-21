ccd03515ee8c349afd40edb123227f500a8329df
Sunny Lim <limhss@gmail.com>
2022-09-21 15:05:24 +0800

    S

--- trello_study.py
+++ trello_study.py
@@ -27,7 +27,7 @@ def displayPDF(file):
         #base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     base64_pdf = base64.b64encode(file).decode('utf-8')
     # Embedding PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     return pdf_display
     # Displaying File
 
